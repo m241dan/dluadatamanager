611e5218070b70143e351aff3f012efb9edc73e7
Daniel R. Koris <korisd@gmail.com>
2016-08-23 22:34:14 -0400

    upgraded data.lua immensely. added default values for the whole data type

--- data.lua
+++ data.lua
@@ -3,22 +3,73 @@
 -- For optimization, data that inherits from this would be    --
 -- best.                                                      --
 ----------------------------------------------------------------
+local LFS = require( "lfs" )
+require( "luautils" )
 
 local D = {}
 
 D.__index = D -- for OOP mimicing
+D.type = "basic"
+D.interp_path = "none"
+D.save_loc = "./"
+D.save_name = "default"
 
-function D:new( i_path )
+function D:new()
    -- OOP stuff
    local d = {}
    setmetatable( d, self )
 
-   -- locals
-   d.i_path = i_path or "none" 	-- interpreter path
-   d.type = "bland"  		-- meaning there is nothing special about this data
-
    return d
    
+end;
+
+function D:save()
+   local f = io.open( D.save_loc, "w" )
+   utils.save( self, f )
+   f:close()
+end;
+
+function D:load( path )
+   local d = dofile( path )
+   setmetatable( d, self )
+
+   return d
+end;
+
+-- This is meant to just be a default fallback. Data types that inherit from this class should define their own serialize method, it'll be faster
+function D:serialize()
+   local lib_name = getmetatable( self )
+   lib_name = table.getKey( package.loaded, lib_name )
+   if( not lib_name ) then
+      return "COULD_NOT_FIND"
+   end
+
+   return lib_name .. ":load( \"" .. self.save_name .. "\" )"
+
+end;
+
+function D:setSaveName( name )
+   if( type( name ) ~= "string" ) then
+      print( "D:setSaveName: argument passed must be a string." )
+      return
+   end
+   self.save_name = name
+end;
+
+function D:setSaveLoc( path )
+   if( not LFS.dir( path ) ) then
+      print( "Directory " .. path .. " does not exist, cannot set as new save location." )
+      return
+   end
+   self.save_loc = path
+end;
+
+function D:setInterp( path )
+   if( not utils.fileExists( path ) ) then
+      print( "File " .. path .. " does not exist, cannot set the new interp." )
+      return   
+   end
+   self.interp_path = path
 end;
 
 return D
